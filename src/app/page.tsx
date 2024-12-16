a1f80a17bb3ed8a86979a3fd59cde3a3838ec416
Alex George <alexander.george03@icloud.com>
2024-12-16 12:07:36 -0500

    centered text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className="flex items-center justify-center h-screen">
         {/* Center content */}
+        <h1 className = "pt-4 p text-center text-4xl">123 mbps</h1>
       </div>
       <div>
 
