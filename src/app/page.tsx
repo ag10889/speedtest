897e8c7bec3a07a51d7dbeacec2d3bd0a23ad748
Alex George <alexander.george03@icloud.com>
2024-12-16 13:51:16 -0500

    Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,9 +94,27 @@ export default function Home() {
 
   return (
     <main>
+      
       <div className="flex items-center justify-center h-1/2">
         {/* Center content */}
-        <h1 className = "mt-64 p text-center text-4xl">123 mbps</h1>
+       
+      <div className="flex justify-center">
+      <h1 className = "mt-64 text-center text-4xl">123 mbps</h1>
+      <button className="bg-red-500 hover:bg-red-600 text-white font-bold p-4 rounded  mt-64">
+      <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          fill="none" 
+          viewBox="0 0 24 24" 
+          strokeWidth="2" 
+          stroke="currentColor" 
+          className="w-6 h-6"
+          strokeLinecap="round" 
+          strokeLinejoin="round"
+        >
+          <path d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+      </button>
+      </div>
       </div>
       <div className="flex justify-center mt-44">
         <LinearSpeedometer />
